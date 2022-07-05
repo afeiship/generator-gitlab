bec30ba8179f192bdd847bd7ca830fa3e142d0f5
afeiship <1290657123@qq.com>
2022-07-05 13:07:05 +0800

    chore: env cmdrc

--- generators/app/templates/.env-cmdrc.js
+++ generators/app/templates/.env-cmdrc.js
@@ -5,6 +5,9 @@ module.exports = CraEnvs.set({
     base_url: 'https://api-beta.demo.com/v1',
   },
   staging: {
+    base_url: 'https://api-staging.demo.com/v1',
+  },
+  production: {
     base_url: 'https://api.demo.com/v1',
   },
 });
